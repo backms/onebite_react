7a90904c6605c9ee4e7b5e8b0995c0bf0bd55e6d
backms <iphonex@kakao.com>
2024-09-09 20:33:55 +0900

    Edit, Diary 컴포넌트

--- Diary/src/pages/Diary.jsx
+++ Diary/src/pages/Diary.jsx
@@ -1,10 +1,42 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import Header from "../components/Header.jsx";
+import Button from "../components/Button.jsx";
+import Viewer from "../components/Viewer.jsx";
+import useDiary from "../hooks/useDiary.jsx";
+import {getStringedDate} from "../util/get-stringed-date.js";
+import usePageTitle from "../hooks/usePageTitle.jsx";
 
 const Diary = () => {
     const params = useParams();
-    console.log(params);
+    const nav = useNavigate();
 
-    return <div>Diary</div>;
+    const curDiaryItem = useDiary(params.id);
+    usePageTitle(`${params.id}번 일기`);
+
+    if(!curDiaryItem){
+        return <div>데이터 로딩중..!</div>;
+    }
+
+    const {createdDate, emotionId, content} = curDiaryItem;
+    const title = getStringedDate(new Date(createdDate));
+
+    return (
+        <div>
+            <Header
+                title={`${title} 기록`}
+                leftChild={
+                    <Button onClick={() => nav(-1)} text={"< 뒤로 가기"}/>
+                }
+                rightChild={
+                    <Button
+                        onClick={() => nav(`/edit/${params.id}`)}
+                        text={"수정하기"}
+                    />
+                }
+            />
+            <Viewer emotionId={emotionId} content={content} />
+        </div>
+    );
   };
   
   export default Diary;
--- Diary/src/pages/Edit.jsx
+++ Diary/src/pages/Edit.jsx
@@ -1,10 +1,59 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import Header from "../components/Header.jsx";
+import Button from "../components/Button.jsx";
+import Editor from "../components/Editor.jsx";
+import { useContext, useEffect, useState } from "react";
+import {DiaryDispatchContext, DiaryStateContext} from "../App.jsx";
+import useDiary from "../hooks/useDiary.jsx";
+import usePageTitle from "../hooks/usePageTitle.jsx";
 
 const Edit = () => {
     const params = useParams();
+    const nav = useNavigate();
+    const { onDelete, onUpdate } = useContext(DiaryDispatchContext);
+
+    const curDiaryItem = useDiary(params.id);
+    usePageTitle(`${params.id}번 일기 수정`);
+
+    const onClickDelete = () => {
+        if(window.confirm("일기를 정말 삭제할까요?")){
+            onDelete(params.id);
+            nav("/", {replace: true});
+        }
+    }
+
+    const onSubmit = (input) => {
+        if(window.confirm("일기를 정말 수정할까요?")) {
+            onUpdate(
+                params.id,
+                input.createdDate.getTime(),
+                input.emotionId,
+                input.content
+            );
+            nav("/", {replace: true});
+        }
+    }
 
     return (
-        <div>{params.id}번 일기입니다.</div>
+        <div>
+            <Header
+                title={"일기 수정하기"}
+                leftChild={
+                    <Button onClick={() => nav(-1)} text={"< 뒤 로가기"} />
+                }
+                rightChild={
+                    <Button
+                        onClick={onClickDelete}
+                        text={"삭제하기"}
+                        type={"NEGATIVE"}
+                    />
+                }
+            />
+            <Editor
+                onSubmit={onSubmit}
+                initData={curDiaryItem}
+            />
+        </div>
     )
 }
 
