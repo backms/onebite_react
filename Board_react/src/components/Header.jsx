eb4a19846b300691923b476bf19fb618623a0e16
backms <iphonex@kakao.com>
2024-09-10 21:38:43 +0900

    - commit 테스트

--- Board_react/src/components/Header.jsx
+++ Board_react/src/components/Header.jsx
@@ -5,6 +5,7 @@ const Header = ({title, leftChild, rightChild}) => {
         <header className="Header">
             <div className="header_left">{leftChild}</div>
             <div className="header_center">{title}</div>
+
             <div className="header_right">{rightChild}</div>
         </header>
     )
