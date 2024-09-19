8cdf9752fbcedfb35292e0ccac753a62935ea0df
backms <iphonex@kakao.com>
2024-09-19 20:13:45 +0900

    Detail

--- Board_react/src/pages/Detail.jsx
+++ Board_react/src/pages/Detail.jsx
@@ -1,11 +1,39 @@
-import { useParams } from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
+import Header from "../components/Header.jsx";
+import Button from "../components/Button.jsx";
+import useBoard from "../hooks/useBoard";
+import usePageTitle from "../hooks/usePageTitle.jsx";
 
 const Detail = () => {
     const params = useParams();
-    console.log(params);
+    const nav = useNavigate();
+
+    const curBoardItem = useBoard(params.id);
+    usePageTitle(`${params.id}번 게시글`);
+
+
+    if(!curBoardItem){
+        return <div>데이터 로딩중</div>;
+    }
+
+    const {createdDate, title, content, writer} = curBoardItem;
 
     return (
-        <div>Detail</div>
+        <div>
+            <Header
+                title={title}
+                leftChild={
+                    <Button onClick={() => nav(-1)} text={"< 뒤로 가기"} />
+                }
+                rightChild={
+                    <Button
+                        onClick={() => nav(`/edit/${params.id}`)}
+                        text={"수정하기"}
+                    />
+                }
+            />
+
+        </div>
     )
 }
 
