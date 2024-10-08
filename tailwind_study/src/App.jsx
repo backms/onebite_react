aa5ddab7c1780d96715b4921504954de1c069e5e
backms <iphonex@kakao.com>
2024-10-08 22:24:08 +0900

    Header 수정

--- tailwind_study/src/App.jsx
+++ tailwind_study/src/App.jsx
@@ -3,11 +3,13 @@ import './App.css'
 import { Routes, Route } from "react-router-dom";
 import Board from "./pages/Board";
 import PageNotFound from "./components/PageNotFound";
+import Header from "./components/Header.jsx";
 
 function App() {
 
   return (
     <>
+        <Header />
         <Routes>
             <Route path="/" element={ <Board/> } />
             <Route path="*" element={ <PageNotFound /> } />
--- tailwind_study/src/pages/Board.jsx
+++ tailwind_study/src/pages/Board.jsx
@@ -2,7 +2,7 @@ import Header from "../components/Header";
 
 const Board = () => {
     return (
-        <Header />
+        <></>
     )
 }
 
